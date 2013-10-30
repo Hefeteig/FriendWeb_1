ff2fce12a6f5271dfec8448e3a97132b3425efb9
Hoff97 <frithjof97@web.de>
2013-10-30 13:25:46 +0100

    Changed Logout

--- logout.php
+++ logout.php
@@ -1,12 +1,13 @@
 <?php
-	$_SESSION = array();
+	session_destroy();
+	/*$_SESSION = array();
 	if (ini_get("session.use_cookies"))
 	{
 		$params = session_get_cookie_params();
 		setcookie(session_name(), '', time() - 42000, $params["path"],
 			$params["domain"], $params["secure"], $params["httponly"]
 		);
-	}
+	}*/
 
 	require_once 'lib/Twig/Autoloader.php';
 	Twig_Autoloader::register();
